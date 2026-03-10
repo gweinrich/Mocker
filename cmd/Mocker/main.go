119fd30c4fe2f04079df9982be7ae5d876937749
gweinrich <gweinrich@smu.edu>
2026-03-10 17:54:51 -0500

    Added basic fork to create container with isolated UTS and PID namespaces

--- cmd/Mocker/main.go
+++ cmd/Mocker/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"syscall"
 
 	"github.com/spf13/cobra"
 )
@@ -14,8 +16,57 @@ var rootCmd = &cobra.Command{
 }
 
 func main() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
+	// "run" is the first argument passed to the program
+	// e.g. "go run main.go run /bin/sh"
+	switch os.Args[1] {
+	case "run":
+		run()
+	case "child":
+		child()
+	}
+}
+
+func run() {
+	// Re-run this same program but with "child" as the argument
+	// This is a common pattern for namespace setup
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	// This is where the magic happens
+	// CLONE_NEWUTS gives the child its own UTS namespace
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS |
+			syscall.CLONE_NEWPID,
+	}
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+}
+
+func child() {
+	// This code runs inside the new namespace
+	// Set a custom hostname - this won't affect the host
+	syscall.Sethostname([]byte("mocker-container"))
+
+	// Run whatever command was passed in
+	// e.g. /bin/sh
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Setsid: true, // Create a new session
+		Ctty:   0,    // Set stdin (fd 0) as the controlling terminal
+	}
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 }
